a78b011387066c66c7ac0c6067a26a386f4e5b61
Sonia Singh <sonia.singh0409@gmail.com>
2021-12-22 01:43:42 +0000

    replace event handler from keydown to input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,28 @@
 
 const $tweetText = $("#tweet-text");
 let count = $(".counter").val()  // will keep track of the nuber of characters
-const $counterOutput = $(".counter")
+const $counterOutput = $(".counter");
 
-$tweetText.on('keydown', (event) => {
-  //count the number of clicks/inputs
 
-  const key = event.key;
-  if (key === "Backspace") {
-    console.log(count);
-    count = count + 1;
-  } else {
-    count--;
-  }
-
+$tweetText.on('input', (event) => {
+  let tweetLength = $tweetText.val().length;
+  //count increment as per 140 minus length of the tweet
+  if(count < 140 - tweetLength || count > 140 - tweetLength ){
+    count = 140 - tweetLength;
+  } else{
+    count = count - 1;
+  } 
   $counterOutput.text(count);
   //conditions: value>0 color black; value < 0 red;
   if (count > 0) {
     $counterOutput.css('color', 'black');
   } else if (count <= 0) {
     $counterOutput.css('color', 'red');
-    //$(this).on()
+
   }
+
 });
+
 $tweetText.focus()
 
 

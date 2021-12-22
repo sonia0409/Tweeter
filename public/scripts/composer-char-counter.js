a8184e67392edfb3f851fb70e52da397c6521497
Sonia Singh <sonia.singh0409@gmail.com>
2021-12-22 01:48:28 +0000

    replace event handler from keydown to input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -7,11 +7,11 @@ const $counterOutput = $(".counter");
 $tweetText.on('input', (event) => {
   let tweetLength = $tweetText.val().length;
   //count increment as per 140 minus length of the tweet
-  if(count < 140 - tweetLength || count > 140 - tweetLength ){
+  if (count < 140 - tweetLength || count > 140 - tweetLength) {
     count = 140 - tweetLength;
-  } else{
+  } else {
     count = count - 1;
-  } 
+  }
   $counterOutput.text(count);
   //conditions: value>0 color black; value < 0 red;
   if (count > 0) {
